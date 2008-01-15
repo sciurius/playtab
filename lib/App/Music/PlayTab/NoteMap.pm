aca1c9dcbe1709739d8475947ddd5552a0d8ee5d
Johan Vromans <jvromans@squirrel.nl>
2008-01-15 15:57:23 +0000

    Add documentation.

--- lib/App/Music/PlayTab/NoteMap.pm
+++ lib/App/Music/PlayTab/NoteMap.pm
@@ -7,20 +7,57 @@ use warnings;
 
 our $VERSION = 0.01;
 
+=head1 NAME
+
+NoteMap - Common data and routines.
+
+=head1 SYNOPSIS
+
+  use App::Music::PlayTab::NoteMap;
+
+=head1 DESCRIPTION
+
+NoteMap contains common data and routines, some of these are exported
+on demand.
+
+@FNotes contains all the note names, using sharps. E.g., $FNotes[3] is
+'D#'. @FNotes can be imported on demand.
+
+@SNotes contains all the note names, using flats. E.g., $FNotes[3] is
+'Eb'. @SNotes can be imported on demand.
+
+Subroutine note_to_key returns the ordinal value (key) for a given
+plain note. It returns undef for an unrecognized argument.
+note_to_key can be imported on demand.
+
+Subroutine key_to_note returns the note for a given key. The optional
+second argument can be used to select the use of sharps or flats. If
+the second argument is not used, the currently selected mode is
+applied.
+key_to_note can be imported on demand.
+
+Subroutines set_flat and set_sharp can be used to select whether note
+names must be produced with flats or sharps.
+
+=cut
+
 use base qw(Exporter);
 our @EXPORT_OK;
 BEGIN {
-    @EXPORT_OK = qw(@FNotes @SNotes note_to_key key_to_note set_sharp set_flat);
+    @EXPORT_OK = qw(@FNotes @SNotes note_to_key key_to_note);
 }
 
+# All notes, using sharps.
 our @SNotes =
   # 0    1    2    3    4    5    6    7    8    9    10   11
-  ('C','C#','D','D#','E','F','F#','G','G#','A','A#','B');
+  ('C', 'C#','D', 'D#','E', 'F', 'F#','G', 'G#','A', 'A#','B');
+
 # All notes, using flats.
 our @FNotes =
   # 0    1    2    3    4    5    6    7    8    9    10   11
-  ('C','Db','D','Eb','E','F','Gb','G','Ab','A','Bb','B');
-# The current mapping.
+  ('C', 'Db','D', 'Eb','E', 'F', 'Gb','G', 'Ab','A', 'Bb','B');
+
+# The current mapping. This can be changed by set_flat/set_sharp.
 my $Notes = \@SNotes;
 
 # Reverse mapping (plain notes only).
