131adbebfc10e3af1497529725e2ef311e5335f7
Johan Vromans <jvromans@squirrel.nl>
2010-05-21 14:41:27 +0000

    Compare files regardless of line endings.

--- t/testscript.pl
+++ t/testscript.pl
@@ -29,25 +29,19 @@ my $ok = !differ ("${prefix}test.ps", "${prefix}${base}.ps");
 unlink ("${prefix}test.ps") if $ok;
 print $ok ? "" : "not ", "ok 6\n";
 
+# Compare two text files, ignoring line endings.
 sub differ {
-    # Perl version of the 'cmp' program.
-    # Returns 1 if the files differ, 0 if the contents are equal.
-    my ($old, $new) = @_;
-    unless ( open (F1, $old) ) {
-	print STDERR ("$old: $!\n");
-	return 1;
-    }
-    unless ( open (F2, $new) ) {
-	print STDERR ("$new: $!\n");
-	return 1;
-    }
-    my ($buf1, $buf2);
-    my ($len1, $len2);
-    while ( 1 ) {
-	$len1 = sysread (F1, $buf1, 10240);
-	$len2 = sysread (F2, $buf2, 10240);
-	return 0 if $len1 == $len2 && $len1 == 0;
-	return 1 if $len1 != $len2 || ( $len1 && $buf1 ne $buf2 );
-    }
+    my ($file1, $file2) = @_;
+    my ($str1, $str2);
+    local($/);
+    open(my $fd1, "<:raw", $file1) or die("$file1: $!\n");
+    $str1 = <$fd1>;
+    close($fd1);
+    open(my $fd2, "<:raw", $file2) or die("$file2: $!\n");
+    $str2 = <$fd2>;
+    close($fd2);
+    $str1 =~ s/[\n\r]+/\n/g;
+    $str2 =~ s/[\n\r]+/\n/g;
+    return 0 if $str1 eq $str2;
+    1;
 }
-
