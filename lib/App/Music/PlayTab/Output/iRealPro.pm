07c24d01c6a5517035aaf07029c6de6ef371034b
Johan Vromans <jvromans@squirrel.nl>
2015-01-19 23:04:05 +0100

    First shots at iRealPro support.

---
+++ lib/App/Music/PlayTab/Output/iRealPro.pm
@@ -0,0 +1,93 @@
+#!/usr/bin/perl
+
+package App::Music::PlayTab::Output::iRealPro;
+
+# Author          : Johan Vromans
+# Created On      : Mon Apr 29 10:53:55 2013
+# Last Modified By: Johan Vromans
+# Last Modified On: Mon Jan 19 15:11:29 2015
+# Update Count    : 178
+# Status          : Unknown, Use with caution!
+
+################ Common stuff ################
+
+use strict;
+use warnings;
+
+our $VERSION = "2.01";
+
+# Package or program libraries, if appropriate.
+# $LIBDIR = $ENV{'LIBDIR'} || '/usr/local/lib/sample';
+# use lib qw($LIBDIR);
+# require 'common.pl';
+
+################ The Process ################
+
+sub new {			# API
+    my ( $pkg, $args ) = @_;
+
+    # Basically, we have multiple backends in here.
+    # ::iRealPro::Perl generates a Perl program that uses the
+    # iRealPro modules to generate iRealPro data, but that can be
+    # modified after generation for specific purposes beyond the scope
+    # of PlayTab.
+    # ::iRealPro::Song is similar to Perl, but uses the more friendly
+    # Song API.
+    # The decision what backend to use is based on the name of the
+    # generator, so we're not supposed to come here.
+
+    use Carp qw(croak);
+    croak("You're not supposed to come here");
+}
+
+################ Subroutines ################
+
+# Some routines for the backends to use.
+
+my %vec2type;
+
+sub vec2type {
+    setup_vec2type() unless %vec2type;
+    $vec2type{$_[0]};
+}
+
+sub setup_vec2type {
+    %vec2type =
+      (
+       "0 3 6"		    => "Min(b5)",
+       "0 3 6 9"	    => "Dim",
+       "0 3 6 10"	    => "Dim7",		# Min7(b5)
+
+       "0 3 7"		    => "Min",
+       "0 3 7 9"	    => "Min6",
+       "0 3 7 10"	    => "Min7",
+       "0 3 7 10 14"	    => "Min9",
+       "0 3 7 10 14 17"	    => "Min11",
+       "0 3 7 10 14 18 21"  => "Min13",
+
+       "0 3 8"		    => "Min(#5)",
+       "0 3 8 10"	    => "Min7(#5)",
+
+       "0 4 6"		    => "Maj(b5)",
+
+       "0 4 7"		    => "Maj",
+       "0 4 7 9"	    => "6",
+       "0 4 7 9 10 14"	    => "6/9",
+       "0 4 7 10"	    => "7",
+       "0 4 7 10 14"	    => "9",
+       "0 4 7 10 14 17"	    => "11",
+       "0 4 7 10 14 18 21"  => "13",
+       "0 4 7 11"	    => "Maj7",
+       "0 4 7 11 14"	    => "Maj9",
+       "0 4 7 11 14 17"	    => "Maj11",
+       "0 4 7 11 14 18 21"  => "Maj13",
+
+       "0 4 8"		    => "Aug",
+
+       "0 5 7"		    => "Sus4",
+
+       "0 7"		    => "Sus2",
+      );
+}
+
+1;
---
+++ lib/App/Music/PlayTab/Output/iRealPro/Song.pm
@@ -0,0 +1,190 @@
+#!/usr/bin/perl
+
+package App::Music::PlayTab::Output::iRealPro::Song;
+
+# Author          : Johan Vromans
+# Created On      : Mon Jan 19 13:05:01 2015
+# Last Modified By: Johan Vromans
+# Last Modified On: Mon Jan 19 15:40:44 2015
+# Update Count    : 28
+# Status          : Unknown, Use with caution!
+
+################ Common stuff ################
+
+use strict;
+use warnings;
+
+our $VERSION = "2.01";
+
+use App::Music::PlayTab::Output::iRealPro;
+
+# Package or program libraries, if appropriate.
+# $LIBDIR = $ENV{'LIBDIR'} || '/usr/local/lib/sample';
+# use lib qw($LIBDIR);
+# require 'common.pl';
+
+################ The Process ################
+
+sub new {			# API
+    my ( $pkg, $args ) = @_;
+    bless { init => 0 }, $pkg
+}
+
+my $tempo = 120;
+my $style = "Rock Ballad";
+my $need_section = 1;
+
+# Init the backend.
+sub setup {			# API
+    my ( $self, $args ) = @_;
+
+    $self->{fh}->print( <<EOD );
+#! perl
+
+use strict;
+use warnings;
+
+#use Music::iRealPro::Song;
+use Music::iRealBook::Song;
+
+EOD
+}
+
+# New page.
+sub setuppage {			# API
+    my ( $self, $title, $stitles ) = @_;
+
+    $self->{fh}->print( <<EOD );
+song "$title";
+# composer "...";
+tempo $tempo;
+EOD
+
+    if ( $stitles ) {
+	$self->{fh}->print( "# ", $_, "\n" ) foreach @$stitles;
+    }
+
+    $self->{fh}->print( <<EOD );
+
+# Set a default style.
+style "$style";
+EOD
+
+    return;			# N/A
+}
+
+sub finish {			# API
+    my $self = shift;
+    $self->{fh}->print("\n");
+}
+
+# New print line.
+sub setupline {			# API
+    my ( $self, $line ) = @_;
+    return;			# N/A
+}
+
+sub chord {			# API
+    my ( $self, $ch, $dup ) = @_;
+
+    $self->{fh}->print( "section \"Section1\";\n\n"), $need_section = 0
+      if $need_section;
+
+    if ( ref($ch) =~ /::/ ) {
+	$self->_render( $ch, $dup );
+	$self->{_prev_chord} = $ch;
+    }
+    elsif ( ref($ch) eq 'ARRAY' ) {
+	my $fun = "render__" . shift(@$ch);
+	$self->$fun( @$ch );
+    }
+    else {
+	my $fun = "render__$ch";
+	$self->$fun($dup);
+    }
+}
+
+my $pdur = -1;
+
+sub _render {
+    my ( $self, $chord, $dup ) = @_;
+
+    my $name = "C";
+    my $dur = $chord->duration;
+    my $type = "Silence";
+
+    if ( $chord->is_rest ) {
+    }
+    else {
+	$name = $chord->{key}->name;
+	$type = vec2type( "@{$chord->{vec}}" );
+    }
+    if ( $dur ) {
+	$dur = int($dur / ($chord->duration_base / 4));
+    }
+    else {
+	$dur = $dup;
+    }
+
+    $type = vec2type( "@{$chord->{vec}}" );
+
+    if ( $type =~ /^M(in|aj)$/ && !$chord->bass ) {
+	my $ir = $chord->{key}->name;
+	$ir =~ s/#$/is/;
+	$ir =~ s/([BDG])b$/$1es/;
+	$ir =~ s/([AE])b$/$1s/;
+	$ir .= "m" if $type eq "Min";
+	$ir .= " " . $dur unless $dur == $pdur;
+	$self->{fh}->print( $ir, "; ");
+    }
+    else {
+	$type .= "/" . $chord->{bass}->[0]->name if $chord->{bass};
+	$self->{fh}->print( "chord ",
+			    "\"$name\", \"$type\", $dur; ");
+    }
+    $pdur = $dur;
+}
+
+sub render__rest { }
+
+sub bar {			# API
+    my ( $self, $first ) = @_;
+    $pdur = -1, $self->{fh}->print("\n") if $first;
+    return;
+}
+
+sub newline {			# API
+    my ( $self, $count ) = @_;
+    return;			# N/A
+}
+
+sub postfix {			# API
+    my ( $self, $text ) = @_;
+    $self->{fh}->printf( "\t# %s\n", $text );
+    return;			# N/A
+}
+
+sub text {			# API
+    my ( $self, $text, $xxmd, $font ) = @_;
+
+    if ( $text =~ /^([ABCD]|Verse|Intro)$/ ) {
+	$self->{fh}->printf( "\n\nsection \"%s\";\n", $text );
+	$need_section = 0;
+    }
+    else {
+	$self->{fh}->printf( "\n# %s\n", $text );
+    }
+
+    return;			# N/A
+}
+
+sub grids {			# API
+    my ( $self, $grids ) = @_;
+    return;			# N/A
+}
+
+sub vec2type {
+    App::Music::PlayTab::Output::iRealPro::vec2type($_[0]) || "";
+}
+
+1;
